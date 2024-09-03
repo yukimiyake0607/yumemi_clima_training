3c8c206ea426188236c556a959639ac140737f29
yukimiyake0607 <yukimiyake38@gmail.com>
2024-09-03 21:51:01 +0900

    refactor: navigateToHomeScreenをunawaitedから非同期処理に変更
    refs navigateを同期処理で実装するとエラーが発生する可能性があるため

--- lib/models/navigate_to_homescreen_mixin.dart
+++ lib/models/navigate_to_homescreen_mixin.dart
@@ -3,28 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_training/ui/screens/home_screen.dart';
 
 mixin NavigateToHomescreenMixin<T extends StatefulWidget> on State<T> {
-  void navigateToHomeScreen() {
-    unawaited(
-      WidgetsBinding.instance.endOfFrame.then(
-        (_) {
+  Future<void> navigateToHomeScreen() async {
+    await WidgetsBinding.instance.endOfFrame.then(
+      (_) {
+        if (!mounted) {
+          return;
+        }
+        Future.delayed(const Duration(milliseconds: 500), () {
           if (!mounted) {
             return;
           }
-          Future.delayed(const Duration(milliseconds: 500), () {
-            if (!mounted) {
-              return;
-            }
-
-            unawaited(
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => HomeScreen(onReturn: navigateToHomeScreen),
-                ),
-              ),
-            );
-          });
-        },
-      ),
+          Navigator.of(context).push(
+            MaterialPageRoute<void>(
+              builder: (_) => HomeScreen(onReturn: navigateToHomeScreen),
+            ),
+          );
+        });
+      },
     );
   }
 }
