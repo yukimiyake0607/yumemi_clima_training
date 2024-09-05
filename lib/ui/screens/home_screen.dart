24a898fe189b0da203a0053e1593c605e631efdb
yukimiyake0607 <yukimiyake38@gmail.com>
2024-09-05 16:50:21 +0900

    add: showDialogにbarrierDismissibleをfalseにして追加

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -44,7 +44,6 @@ class _HomeScreenState extends State<HomeScreen> {
     await showDialog<void>(
       context: context,
       barrierDismissible: false,
-
       builder: (context) {
         return AlertDialog(
           title: Text(errorMessage),
