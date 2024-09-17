67ff48fcd1d310b28b70b6c6d3ae008a4e910bf1
yukimiyake0607 <yukimiyake38@gmail.com>
2024-09-17 22:24:36 +0900

    add: Mixin内でinitStateを定義
    refs 使用先であるLoadingScreenではinitStateを定義しない

--- lib/models/navigate_to_homescreen_mixin.dart
+++ lib/models/navigate_to_homescreen_mixin.dart
@@ -2,23 +2,13 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 mixin AfterLayoutMixin<T extends StatefulWidget> on State<T> {
-  Future<void> navigateToHomeScreen() async {
-    await WidgetsBinding.instance.endOfFrame.then(
-      (_) {
-        if (!mounted) {
-          return;
-        }
-        Future.delayed(const Duration(milliseconds: 500), () {
-          if (!mounted) {
-            return;
-          }
-          Navigator.of(context).push(
-            MaterialPageRoute<void>(
-              builder: (_) => HomeScreen(onReturn: navigateToHomeScreen),
-            ),
-          );
-        });
-      },
+  @override
+  void initState() {
+    super.initState();
+    unawaited(
+      WidgetsBinding.instance.endOfFrame.then(
+        (_) => navigateToHomeScreen(),
+      ),
     );
   }
 
