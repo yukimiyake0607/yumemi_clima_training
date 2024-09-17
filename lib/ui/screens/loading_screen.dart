f5b5d1d2a74949337207333806ca2019ea5176a2
yukimiyake0607 <yukimiyake38@gmail.com>
2024-09-17 22:26:36 +0900

    refactor: Mixinフォルダを新規作成してMixinを管理

--- lib/ui/screens/loading_screen.dart
+++ lib/ui/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_training/models/navigate_to_homescreen_mixin.dart';
+import 'package:flutter_training/ui/mixins/navigate_to_homescreen_mixin.dart';
 import 'package:flutter_training/ui/screens/home_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
