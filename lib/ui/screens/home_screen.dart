5ad14663d9e0b8c2f9ea33fbb81007f04b7a50d8
yukimiyake0607 <yukimiyake38@gmail.com>
2024-09-17 16:31:12 +0900

    refactor: onReturn変数を削除
    refs LoadingScreenに遷移するメソッドをLoadingScreenの責務にするため、Closeボタンから削除

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -13,6 +13,7 @@ import 'package:yumemi_weather/yumemi_weather.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
