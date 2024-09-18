c708b07adf2723af92c26599587e208ab3e573b2
yukimiyake0607 <yukimiyake38@gmail.com>
2024-09-18 10:47:20 +0900

    refactor: factoryメソッドの引数をdynamicからStringに変更
    refs dynamicでは型安全性が低いため

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -13,7 +13,6 @@ import 'package:yumemi_weather/yumemi_weather.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
