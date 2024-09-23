0bb9a7c104dd1fae3a0cd906c39fb790d8e0137e
yukimiyake0607 <yukimiyake38@gmail.com>
2024-09-23 21:22:54 +0900

    refactor: weather_datastoreを削除し、weather_repositoryに内容を移動

--- lib/data/repository/weather_repository.dart
+++ lib/data/repository/weather_repository.dart
@@ -1,17 +1,17 @@
 import 'dart:convert';
 
-import 'package:flutter_training/data/datastore/weather_datastore.dart';
 import 'package:flutter_training/models/response/weather_condition_response.dart';
 import 'package:flutter_training/models/weather_request.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
 
 class WeatherRepository {
-  WeatherRepository(this.weatherDatastore);
-  final WeatherDatastore weatherDatastore;
+  WeatherRepository(this._yumemiWeather);
+  final YumemiWeather _yumemiWeather;
 
   Future<WeatherConditionResponse> getWeather() async {
     final weatherRequest = WeatherRequest(area: 'tokyo', date: DateTime.now());
-    final weatherDataOfJson =
-        await weatherDatastore.fetchWeather(weatherRequest);
+    final request = jsonEncode(weatherRequest);
+    final weatherDataOfJson = _yumemiWeather.fetchWeather(request);
     final response = jsonDecode(weatherDataOfJson) as Map<String, dynamic>;
     final weatherData = WeatherConditionResponse.fromJson(response);
     return weatherData;
