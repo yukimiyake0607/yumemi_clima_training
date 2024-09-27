a808b33b524af47695a31adfd38c53d43da6cb90
yukimiyake0607 <yukimiyake38@gmail.com>
2024-09-28 00:34:23 +0900

    refactor: import部を整理

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,14 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:flutter_training/data/repository/weather_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_training/data/provider/weather_notifier_provider.dart';
 import 'package:flutter_training/models/response/weather_condition_response.dart';
-import 'package:flutter_training/models/weather_condition.dart';
 import 'package:flutter_training/ui/extensions/api_error_ext.dart';
-import 'package:flutter_training/ui/extensions/weather_condition_ext.dart';
-import 'package:flutter_training/ui/widgets/button_row.dart';
-import 'package:flutter_training/ui/widgets/temperature_row.dart';
+import 'package:flutter_training/ui/widgets/weather_widget.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 class HomeScreen extends StatefulWidget {
