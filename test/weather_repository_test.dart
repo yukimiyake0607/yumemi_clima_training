5e026de62b7c58d714c5eb65923d43542a99a4c4
yukimiyake0607 <yukimiyake38@gmail.com>
2024-10-06 17:33:03 +0900

    add: toJsonStringのテストを追加
    refs 与えられたデータを正確にJSON形式の文字列に変換できているか確認するため

--- test/weather_repository_test.dart
+++ test/weather_repository_test.dart
@@ -1,12 +1,22 @@
-import 'dart:convert';
-
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/data/repository/weather_repository.dart';
 import 'package:flutter_training/models/weather_request.dart';
+import 'mock/mock.mocks.dart';
 
 void main() {
-  test('WeatherRequest is correctly encoded', () {
-    final weatherRequest =
-        WeatherRequest(area: 'tokyo', date: DateTime(2024, 10, 4));
-    final jsonString = jsonEncode(weatherRequest);
+  group('WeatherRepository tests group', () {
+    test('WeatherRequest is correctly encoded', () {
+      final mockYumemiWeather = MockYumemiWeather();
+      final weatherRepository = WeatherRepository(mockYumemiWeather);
+      final weatherRequest =
+          WeatherRequest(area: 'tokyo', date: DateTime(2024, 10, 4));
+
+      final weatherJsonData = weatherRepository.toJsonString(weatherRequest);
+
+      expect(
+        weatherJsonData,
+        '{"area":"tokyo","date":"2024-10-04 00:00:00.000"}',
+      );
+    });
   });
 }
