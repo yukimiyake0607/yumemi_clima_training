d8e32f7910d3628817e095408e270689f4e61c0e
yukimiyake0607 <yukimiyake38@gmail.com>
2024-10-06 21:24:58 +0900

    add: 無効なデータの入力時は例外を投げるかのテストを追加
    refs jsonDecodeの引数に無効なデータが入力された際、FormatExceptionを投げるかの確認

--- test/weather_repository_test.dart
+++ test/weather_repository_test.dart
@@ -6,16 +6,55 @@ import 'mock/mock.mocks.dart';
 void main() {
   group('WeatherRepository tests group', () {
     test('WeatherRequest is correctly encoded', () {
+      // Arrange
       final mockYumemiWeather = MockYumemiWeather();
       final weatherRepository = WeatherRepository(mockYumemiWeather);
       final weatherRequest =
           WeatherRequest(area: 'tokyo', date: DateTime(2024, 10, 4));
 
+      //Act
       final weatherJsonData = weatherRepository.toJsonString(weatherRequest);
 
+      // Assert
       expect(
         weatherJsonData,
         '{"area":"tokyo","date":"2024-10-04 00:00:00.000"}',
+      );
+    });
+
+    test('toMap is decoded correctly', () {
+      // Arrange
+      const weatherDataOfJson =
+          '{"weather_condition":"cloudy","max_temperature":25,"min_temperature":7,"date":"2020-04-01T12:00:00+09:00"}';
+      final mockYumemiWeather = MockYumemiWeather();
+      final weatherRepository = WeatherRepository(mockYumemiWeather);
+
+      // Act
+      final request = weatherRepository.toMap(weatherDataOfJson);
+
+      // Assert
+      final matcher = {
+        'weather_condition': 'cloudy',
+        'max_temperature': 25,
+        'min_temperature': 7,
+        'date': '2020-04-01T12:00:00+09:00',
+      };
+      expect(request, matcher);
+    });
+
+    test('If invalid data is entered, toMap will throw an exception', () {
+      // Arrange
+      final mockYumemiWeather = MockYumemiWeather();
+      final weatherRepository = WeatherRepository(mockYumemiWeather);
+
+      // Act
+      // 無効な入力
+      const invalidData = 'invalid_data';
+
+      // Assert
+      expect(
+        weatherRepository.toMap(invalidData),
+        throwsA(isA<FormatException>()),
       );
     });
   });
