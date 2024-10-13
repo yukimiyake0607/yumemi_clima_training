90b343617bcf735e4dc9f1305f80e6341683ab98
yukimiyake0607 <yukimiyake38@gmail.com>
2024-10-13 21:00:10 +0900

    add: WeatherUsecaseを新規作成
    refs WeatherRepositoryから受け取った値に応じてResultクラスを適用

---
+++ lib/data/usecase/weather_usecase.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_training/data/repository/weather_repository.dart';
+import 'package:flutter_training/models/response/weather_condition_response.dart';
+import 'package:flutter_training/models/result/result.dart';
+import 'package:flutter_training/models/weather_request.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:yumemi_weather/yumemi_weather.dart';
+
+part 'weather_usecase.g.dart';
+
+@riverpod
+WeatherUsecase weatherUsecase(WeatherUsecaseRef ref) {
+  final weatherRepository = ref.watch(weatherRepositoryProvider);
+  return WeatherUsecase(weatherRepository);
+}
+
+class WeatherUsecase {
+  const WeatherUsecase(this.weatherRepository);
+  final WeatherRepository weatherRepository;
+
+  Future<Result<WeatherConditionResponse, YumemiWeatherError>> getWeather(
+    WeatherRequest request,
+  ) async {
+    try {
+      final weatherData = await weatherRepository.getWeather(request);
+      return Result.success(weatherData);
+    } on YumemiWeatherError catch (e) {
+      return Result.failure(e);
+    }
+  }
+}
---
+++ lib/data/usecase/weather_usecase.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: type=lint
+
+part of 'weather_usecase.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$weatherUsecaseHash() => r'c434f94caff5ea2b87f02c79813f7c4270d8486d';
+
+/// See also [weatherUsecase].
+@ProviderFor(weatherUsecase)
+final weatherUsecaseProvider = AutoDisposeProvider<WeatherUsecase>.internal(
+  weatherUsecase,
+  name: r'weatherUsecaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$weatherUsecaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef WeatherUsecaseRef = AutoDisposeProviderRef<WeatherUsecase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
