03a21a6be4389dcb48537e820555e7b013e9456a
yukimiyake0607 <yukimiyake38@gmail.com>
2024-10-23 07:37:49 +0900

    add: 曇りの画像が表示されるテストを追加

--- test/widget/home_screen_test.dart
+++ test/widget/home_screen_test.dart
@@ -15,7 +15,7 @@ import '../mock/weather_usecase_mock.mocks.dart';
 final mockWeatherUsecase = MockWeatherUsecase();
 
 void main() {
-  group('description', () {
+  group('WidgetTests of Home Screen', () {
     testWidgets('Display a sunny image', (tester) async {
       // Arrange
       const weatherResponse = WeatherResponse(
@@ -59,6 +59,50 @@ void main() {
         (svgPicture.bytesLoader as SvgAssetLoader).assetName,
         equals('assets/sunny.svg'),
       );
+    });
+
+    testWidgets('display a cloudy image', (tester) async {
+      // Arrange
+      // ウィジェットツリーを生成
+      await tester.pumpWidget(
+        ProviderScope(
+          overrides: [
+            weatherUsecaseProvider.overrideWithValue(mockWeatherUsecase),
+          ],
+          child: const MaterialApp(
+            home: HomeScreen(),
+          ),
+        ),
+      );
+
+      // 天気情報のデータを設定
+      const response = WeatherResponse(
+        weatherCondition: WeatherCondition.cloudy,
+        maxTemperature: 30,
+        minTemperature: 20,
+      );
+      when(mockWeatherUsecase.getWeather(any)).thenAnswer(
+        (_) async =>
+            const Result<WeatherResponse, YumemiWeatherError>.success(response),
+      );
+
+      // 初期状態のHomeScreenにPlaceholderが表示されているか確認
+      expect(find.byType(Placeholder), findsOneWidget);
+
+      // Act
+      await tester.tap(find.widgetWithText(TextButton, 'Reload'));
+      await tester.pump();
+
+      // Assert
+      // SvgPictureウィジェットが存在するか
+      expect(find.byType(SvgPicture), findsOneWidget);
+
+      final svgPicture = tester.widget<SvgPicture>(find.byType(SvgPicture));
+
+      // svgPictureがアセットから読み込まれているか確認
+      expect(svgPicture.bytesLoader, isA<SvgAssetLoader>());
+      final svgAsset = svgPicture.bytesLoader as SvgAssetLoader;
+      expect(svgAsset.assetName, equals('assets/cloudy.svg'));
     });
   });
 }
