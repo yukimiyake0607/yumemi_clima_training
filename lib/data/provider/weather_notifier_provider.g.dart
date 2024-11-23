85a259a3786385f223267f626d7524a27ba26b6f
yukimiyake0607 <yukimiyake38@gmail.com>
2024-11-23 18:04:13 +0900

    rename: YumemiWeatherのmockファイル名を変更

--- lib/data/provider/weather_notifier_provider.g.dart
+++ lib/data/provider/weather_notifier_provider.g.dart
@@ -24,4 +24,5 @@ final weatherNotifierProvider =
 );
 
 typedef _$WeatherNotifier = AutoDisposeAsyncNotifier<WeatherResponse>;
+// ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
