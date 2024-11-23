f82a286059f5fdca4d9dff322d39926cde683fbe
yukimiyake0607 <yukimiyake38@gmail.com>
2024-11-23 18:04:13 +0900

    rename: YumemiWeatherのmockファイル名を変更

--- lib/data/usecase/weather_usecase.g.dart
+++ lib/data/usecase/weather_usecase.g.dart
@@ -23,4 +23,5 @@ final weatherUsecaseProvider = AutoDisposeProvider<WeatherUsecase>.internal(
 );
 
 typedef WeatherUsecaseRef = AutoDisposeProviderRef<WeatherUsecase>;
+// ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
